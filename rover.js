22be0fcc1906f4b0487a6a349fb885b8369e0a89
calambre8@gmail.com <calambre8@gmail.com>
2023-03-20 20:29:53 +0100

    apiNasa

---
+++ rover.js
@@ -0,0 +1,10 @@
+const rover = document.querySelector("#rover");
+const data = document.querySelector("#data");
+const btnCarrega = document.querySelector("#btnCarrega");
+const camara = document.querySelector("#camara");
+
+var URL = `https://api.nasa.gov/mars-photos/api/v1/${rover}/curiosity/photos?sol=1000&api_key=DEMO_KEY`
+
+function camara(){
+
+}
