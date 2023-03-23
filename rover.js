458eec343c6194018e7f48ba926c02fa28fa8a29
calambre8@gmail.com <calambre8@gmail.com>
2023-03-23 20:38:44 +0100

    roverSS

--- rover.js
+++ rover.js
@@ -4,7 +4,19 @@ const btnCarrega = document.querySelector("#btnCarrega");
 const camara = document.querySelector("#camara");
 
 var URL = `https://api.nasa.gov/mars-photos/api/v1/${rover}/curiosity/photos?sol=1000&api_key=DEMO_KEY`
+const selectorCamaraCuriosity = [FHAZ, RHAZ, MAST, CHEMCAM, MAHLI, MARDI, NAVCAM]
+const selectorCamaraOportunitySpirit = [FHAZ, RHAZ, NAVCAM, PANCAM, MINITES]
 
-function camara(){
+btnCarrega.onclick = carrega;
+
+function camara() {
+    var recorArray = []
+    if (rover.ariaValueMax.toLocaleLowerCase == "Curiosity") {
+
+    }
+
+
+}
+function carrega() {
 
 }
