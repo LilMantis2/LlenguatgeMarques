29d92684e31fe0863c550cab2a0793e12e91d1c1
calambre8@gmail.com <calambre8@gmail.com>
2023-04-27 20:41:03 +0200

    exercicis actualitzat

--- PracticaXML/practicaPelXML.js
+++ PracticaXML/practicaPelXML.js
@@ -14,6 +14,9 @@ function carregaXML() {
       exercici2(xml);
       exercici3(xml);
       exercici4(xml);
+      exercici5(xml);
+      exercici6(xml);
+      exercici9(xml);
     })
     .catch(console.error);
 }
@@ -28,29 +31,89 @@ function exercici2(xml) {
   let peliculas = xml.querySelectorAll("pelicula");
   peliculas.forEach(function (pelicula) {
     if (pelicula.querySelector("anio").textContent >= 2001) {
-      resultado += pelicula.querySelector("titulo").textContent + "\n";
+      resultado += "<li>" + pelicula.querySelector("titulo").textContent + "</li>";
     }
   });
-  document.querySelector("#ex2").textContent
-    = "-" + resultado;
-
-
+  document.querySelector("#ex2").innerHTML = "<ol>" + resultado + "</ol>";
 }
-function exercici3(xml){
+
+function exercici3(xml) {
   let resultado = "";
   let peliculasBluRay = xml.querySelectorAll("pelicula");
-  peliculasBluRay.forEach(function(peliculaBlu){
-    if(peliculaBlu.getAttribute("formato") == "Blu-ray"){
-      resultado += peliculaBlu.querySelector("titulo").textContent + "\n";
+  peliculasBluRay.forEach(function (peliculaBlu) {
+    if (peliculaBlu.querySelector("titulo").getAttribute("formato") == "Blu-ray") {
+      resultado += "<li>" + peliculaBlu.querySelector("titulo").textContent + "</li>";
     }
   });
-  document.querySelector("#ex3").textContent = "-" + resultado;
+  document.querySelector("#ex3").innerHTML = "<ol>" + resultado + "</ol>";
 }
 
 
-function exercici4(xml){
+function exercici4(xml) {
+  let resultado = "";
+  let pelDvdSegle = xml.querySelectorAll("pelicula");
+  pelDvdSegle.forEach(function (peliculaDS) {
+    if (peliculaDS.querySelector("titulo").getAttribute("formato") == "DVD" ||
+      peliculaDS.querySelector("anio").textContent >= 2001) {
+      resultado += "<li>" + peliculaDS.querySelector("titulo").textContent + "</li>";
+    }
+  });
+  document.querySelector("#ex4").innerHTML = "<ol>" + resultado + "</ol>";
+}
+
+function exercici5(xml) {
+  let resultado = "";
+  let nomActors = xml.querySelectorAll("pelicula");
+  nomActors.forEach(function (nomAct) {
+    if (nomAct.querySelector("titulo").getAttribute("id") == "6") {
+      resultado += "<li>" + nomAct.querySelector("actor").textContent + "</li>";
+    }
+  });
+  document.querySelector("#ex5").innerHTML = "<ol>" + resultado + "</ol>";
 
 }
+
+function exercici6(xml){
+   let resultado = "";
+   let sinopsis = xml.querySelectorAll("pelicula");
+   sinopsis.forEach(function(sinop) {
+    if(sinop.querySelector("titulo").textContent == "Forrest Gump"){
+      resultado = sinop.querySelector("sinopsis").textContent;
+    }
+   });
+
+   document.querySelector("#ex6").innerHTML =  resultado;
+}
+
+function exercici9(xml) {
+  let generos = xml.querySelectorAll("genero");
+  let mapa = new Map();
+
+  generos.forEach(element => {
+    let genero = element.textContent;
+
+    if (mapa.has(genero)) {
+      let valor = mapa.get(genero);
+      mapa.set(genero, valor + 1);
+    } else {
+
+      mapa.set(genero, 1);
+    }
+  });
+
+  let genResultado = "";
+  let valorMax = 0;
+  mapa.forEach((value, key) =>{
+      if(value > valorMax){
+        valorMax = value;
+        genResultado = key;
+      }
+  });
+
+
+  document.querySelector("#ex9").textContent = `El genero más visto es ${genResultado} amb un total de ${valorMax}`;
+}
+
 
 
 
